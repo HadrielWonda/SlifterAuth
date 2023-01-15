88780a5304e4e0c5f35369f6071de63fa36c2312
Olawale Abaniwonda <olawalebash2017@gmail.com>
2023-01-15 22:33:37 +0100

    Create AuthorizeViews.cs

---
+++ Authorization/AuthorizeViews.cs
@@ -0,0 +1,27 @@
+
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace SlifterAuth.Authorization;
+
+
+public class AuthorizeView : AuthorizeViewCore
+{
+    private readonly IAuthorizeData[] selfAsAuthorizeData;
+
+    
+    public AuthorizeView()
+    {
+        selfAsAuthorizeData = new[] { new AuthorizeDataAdapter(this) };
+    }
+
+    
+    [Parameter] public string? Policy { get; set; }
+
+   
+    [Parameter] public string? Roles { get; set; }
+
+    
+    protected override IAuthorizeData[] GetAuthorizeData()
+        => selfAsAuthorizeData;
+}
