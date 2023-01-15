4ac98e641c3616a1588a67c287b29df849dd1395
Olawale Abaniwonda <olawalebash2017@gmail.com>
2023-01-15 22:27:33 +0100

    Create AuthorizeDataAdapters.cs

---
+++ Authorization/AuthorizeDataAdapters.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Microsoft.AspNetCore.Components.Authorization;
+
+
+internal sealed class AuthorizeDataAdapter : IAuthorizeData
+{
+    private readonly AuthorizeView _component;
+
+    public AuthorizeDataAdapter(AuthorizeView component)
+    {
+        _component = component ?? throw new ArgumentNullException(nameof(component));
+    }
+
+    public string? Policy
+    {
+        get => _component.Policy;
+        set => throw new NotSupportedException();
+    }
+
+    public string? Roles
+    {
+        get => _component.Roles;
+        set => throw new NotSupportedException();
+    }
+
+  
+    public string? AuthenticationSchemes
+    {
+        get => null;
+        set => throw new NotSupportedException();
+    }
+}
