844e960fe6a0d706d9584ef98fac607a0a5cf89e
Olawale Abaniwonda <olawalebash2017@gmail.com>
2023-01-17 19:01:11 +0100

    Create JwtBearerHandler.cs

---
+++ Authentication/JwtBearer/JwtBearerHandler.cs
@@ -0,0 +1,311 @@
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+
+namespace Microsoft.AspNetCore.Authentication.JwtBearer;
+
+
+public class JwtBearerHandler : AuthenticationHandler<JwtBearerOptions>
+{
+    private OpenIdConnectConfiguration? _configuration;
+
+  
+    public JwtBearerHandler(IOptionsMonitor<JwtBearerOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+        : base(options, logger, encoder, clock)
+    { }
+
+  
+    protected new JwtBearerEvents Events
+    {
+        get => (JwtBearerEvents)base.Events!;
+        set => base.Events = value;
+    }
+
+    /// <inheritdoc />
+    protected override Task<object> CreateEventsAsync() => Task.FromResult<object>(new JwtBearerEvents());
+
+    
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        string? token;
+        try
+        {
+            await Events.MessageReceived(messageReceivedContext);
+            if (messageReceivedContext.Result != null)
+            {
+                return messageReceivedContext.Result;
+            }
+            
+            token = messageReceivedContext.Token;
+
+            if (string.IsNullOrEmpty(token))
+            {
+                string authorization = Request.Headers.Authorization.ToString();
+
+                if (string.IsNullOrEmpty(authorization))
+                {
+                    return AuthenticateResult.NoResult();
+                }
+
+                if (authorization.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+                {
+                    token = authorization.Substring("Bearer ".Length).Trim();
+                }
+
+                // If no token found, no further work possible
+                if (string.IsNullOrEmpty(token))
+                {
+                    return AuthenticateResult.NoResult();
+                }
+            }
+
+            if (_configuration == null && Options.ConfigurationManager != null)
+            {
+                _configuration = await Options.ConfigurationManager.GetConfigurationAsync(Context.RequestAborted);
+            }
+
+            var validationParameters = Options.TokenValidationParameters.Clone();
+            if (_configuration != null)
+            {
+                var issuers = new[] { _configuration.Issuer };
+                validationParameters.ValidIssuers = validationParameters.ValidIssuers?.Concat(issuers) ?? issuers;
+
+                validationParameters.IssuerSigningKeys = validationParameters.IssuerSigningKeys?.Concat(_configuration.SigningKeys)
+                    ?? _configuration.SigningKeys;
+            }
+
+            List<Exception>? validationFailures = null;
+            SecurityToken? validatedToken = null;
+            foreach (var validator in Options.SecurityTokenValidators)
+            {
+                if (validator.CanReadToken(token))
+                {
+                    ClaimsPrincipal principal;
+                    try
+                    {
+                        principal = validator.ValidateToken(token, validationParameters, out validatedToken);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.TokenValidationFailed(ex);
+                        
+                        if (Options.RefreshOnIssuerKeyNotFound && Options.ConfigurationManager != null
+                            && ex is SecurityTokenSignatureKeyNotFoundException)
+                        {
+                            Options.ConfigurationManager.RequestRefresh();
+                        }
+
+                        if (validationFailures == null)
+                        {
+                            validationFailures = new List<Exception>(1);
+                        }
+                        validationFailures.Add(ex);
+                        continue;
+                    }
+
+                    Logger.TokenValidationSucceeded();
+
+                    var tokenValidatedContext = new TokenValidatedContext(Context, Scheme, Options)
+                    {
+                        Principal = principal,
+                        SecurityToken = validatedToken
+                    };
+
+                    tokenValidatedContext.Properties.ExpiresUtc = GetSafeDateTime(validatedToken.ValidTo);
+                    tokenValidatedContext.Properties.IssuedUtc = GetSafeDateTime(validatedToken.ValidFrom);
+
+                    await Events.TokenValidated(tokenValidatedContext);
+                    if (tokenValidatedContext.Result != null)
+                    {
+                        return tokenValidatedContext.Result;
+                    }
+
+                    if (Options.SaveToken)
+                    {
+                        tokenValidatedContext.Properties.StoreTokens(new[]
+                        {
+                                new AuthenticationToken { Name = "access_token", Value = token }
+                            });
+                    }
+
+                    tokenValidatedContext.Success();
+                    return tokenValidatedContext.Result!;
+                }
+            }
+
+            if (validationFailures != null)
+            {
+                var authenticationFailedContext = new AuthenticationFailedContext(Context, Scheme, Options)
+                {
+                    Exception = (validationFailures.Count == 1) ? validationFailures[0] : new AggregateException(validationFailures)
+                };
+
+                await Events.AuthenticationFailed(authenticationFailedContext);
+                if (authenticationFailedContext.Result != null)
+                {
+                    return authenticationFailedContext.Result;
+                }
+
+                return AuthenticateResult.Fail(authenticationFailedContext.Exception);
+            }
+
+            return AuthenticateResults.ValidatorNotFound;
+        }
+        catch (Exception ex)
+        {
+            Logger.ErrorProcessingMessage(ex);
+
+            var authenticationFailedContext = new AuthenticationFailedContext(Context, Scheme, Options)
+            {
+                Exception = ex
+            };
+
+            await Events.AuthenticationFailed(authenticationFailedContext);
+            if (authenticationFailedContext.Result != null)
+            {
+                return authenticationFailedContext.Result;
+            }
+
+            throw;
+        }
+    }
+
+    private static DateTime? GetSafeDateTime(DateTime dateTime)
+    {
+        // Assigning DateTime.MinValue or default(DateTime) to a DateTimeOffset when in a UTC+X timezone will throw
+        // Since I don't really care about DateTime.MinValue in this case let's just set the field to null
+        if (dateTime == DateTime.MinValue)
+        {
+            return null;
+        }
+        return dateTime;
+    }
+
+  
+    protected override async Task HandleChallengeAsync(AuthenticationProperties properties)
+    {
+        var authResult = await HandleAuthenticateOnceSafeAsync();
+        var eventContext = new JwtBearerChallengeContext(Context, Scheme, Options, properties)
+        {
+            AuthenticateFailure = authResult?.Failure
+        };
+
+        // Avoid returning error=invalid_token if the error is not caused by an authentication failure (e.g missing token).
+        if (Options.IncludeErrorDetails && eventContext.AuthenticateFailure != null)
+        {
+            eventContext.Error = "invalid_token";
+            eventContext.ErrorDescription = CreateErrorDescription(eventContext.AuthenticateFailure);
+        }
+
+        await Events.Challenge(eventContext);
+        if (eventContext.Handled)
+        {
+            return;
+        }
+
+        Response.StatusCode = 401;
+
+        if (string.IsNullOrEmpty(eventContext.Error) &&
+            string.IsNullOrEmpty(eventContext.ErrorDescription) &&
+            string.IsNullOrEmpty(eventContext.ErrorUri))
+        {
+            Response.Headers.Append(HeaderNames.WWWAuthenticate, Options.Challenge);
+        }
+        else
+        {
+         
+            var builder = new StringBuilder(Options.Challenge);
+            if (Options.Challenge.IndexOf(' ') > 0)
+            {
+                // Only add a comma after the first param, if any
+                builder.Append(',');
+            }
+            if (!string.IsNullOrEmpty(eventContext.Error))
+            {
+                builder.Append(" error=\"");
+                builder.Append(eventContext.Error);
+                builder.Append('\"');
+            }
+            if (!string.IsNullOrEmpty(eventContext.ErrorDescription))
+            {
+                if (!string.IsNullOrEmpty(eventContext.Error))
+                {
+                    builder.Append(',');
+                }
+
+                builder.Append(" error_description=\"");
+                builder.Append(eventContext.ErrorDescription);
+                builder.Append('\"');
+            }
+            if (!string.IsNullOrEmpty(eventContext.ErrorUri))
+            {
+                if (!string.IsNullOrEmpty(eventContext.Error) ||
+                    !string.IsNullOrEmpty(eventContext.ErrorDescription))
+                {
+                    builder.Append(',');
+                }
+
+                builder.Append(" error_uri=\"");
+                builder.Append(eventContext.ErrorUri);
+                builder.Append('\"');
+            }
+
+            Response.Headers.Append(HeaderNames.WWWAuthenticate, builder.ToString());
+        }
+    }
+
+    protected override Task HandleForbiddenAsync(AuthenticationProperties properties)
+    {
+        var forbiddenContext = new ForbiddenContext(Context, Scheme, Options);
+        Response.StatusCode = 403;
+        return Events.Forbidden(forbiddenContext);
+    }
+
+    private static string CreateErrorDescription(Exception authFailure)
+    {
+        IReadOnlyCollection<Exception> exceptions;
+        if (authFailure is AggregateException agEx)
+        {
+            exceptions = agEx.InnerExceptions;
+        }
+        else
+        {
+            exceptions = new[] { authFailure };
+        }
+
+        var messages = new List<string>(exceptions.Count);
+
+        foreach (var ex in exceptions)
+        {
+            string? message = ex switch
+            {
+                SecurityTokenInvalidAudienceException stia => $"The audience '{stia.InvalidAudience ?? "(null)"}' is invalid",
+                SecurityTokenInvalidIssuerException stii => $"The issuer '{stii.InvalidIssuer ?? "(null)"}' is invalid",
+                SecurityTokenNoExpirationException _ => "The token has no expiration",
+                SecurityTokenInvalidLifetimeException stil => "The token lifetime is invalid; NotBefore: "
+                    + $"'{stil.NotBefore?.ToString(CultureInfo.InvariantCulture) ?? "(null)"}'"
+                    + $", Expires: '{stil.Expires?.ToString(CultureInfo.InvariantCulture) ?? "(null)"}'",
+                SecurityTokenNotYetValidException stnyv => $"The token is not valid before '{stnyv.NotBefore.ToString(CultureInfo.InvariantCulture)}'",
+                SecurityTokenExpiredException ste => $"The token expired at '{ste.Expires.ToString(CultureInfo.InvariantCulture)}'",
+                SecurityTokenSignatureKeyNotFoundException _ => "The signature key was not found",
+                SecurityTokenInvalidSignatureException _ => "The signature is invalid",
+                _ => null,
+            };
+
+            if (message is not null)
+            {
+                messages.Add(message);
+            }
+        }
+
+        return string.Join("; ", messages);
+    }
+}
