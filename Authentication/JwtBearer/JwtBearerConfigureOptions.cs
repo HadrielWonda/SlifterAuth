198a26d53e9ce82df67954f80823b3e4800f3c8d
Olawale Abaniwonda <olawalebash2017@gmail.com>
2023-01-17 18:51:12 +0100

    Update JwtBearerConfigureOptions.cs

--- Authentication/JwtBearer/JwtBearerConfigureOptions.cs
+++ Authentication/JwtBearer/JwtBearerConfigureOptions.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
-namespace Microsoft.AspNetCore.Authentication;
+namespace SlifterAuth.Authentication;
 
 internal sealed class JwtBearerConfigureOptions : IConfigureNamedOptions<JwtBearerOptions>
 {
