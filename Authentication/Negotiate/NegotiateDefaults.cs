23d3c78e64f8074a190dc0be8092345228070ebf
Olawale Abaniwonda <olawalebash2017@gmail.com>
2023-02-05 22:50:42 +0100

    Create NegotiateDefaults.cs

---
+++ Authentication/Negotiate/NegotiateDefaults.cs
@@ -0,0 +1,12 @@
+namespace SlifterAuth.Authentication.Negotiate;
+
+/// <summary>
+/// Default values used by Negotiate authentication.
+/// </summary>
+public static class NegotiateDefaults
+{
+    /// <summary>
+    /// Default value for AuthenticationScheme used to identify the Negotiate auth handler.
+    /// </summary>
+    public const string AuthenticationScheme = "Negotiate";
+}
